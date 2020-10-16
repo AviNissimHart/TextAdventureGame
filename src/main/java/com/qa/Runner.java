02f649cfdfd249343e7d202223bec44ded2d1876
AviNissimHart <ahart@qa.com>
2020-10-16 11:57:45 +0100

    Made a starting stage taking in name and action. Beginning a battle stage similar to one i found online

---
+++ src/main/java/com/qa/Runner.java
@@ -0,0 +1,22 @@
+package com.qa;
+
+import java.util.Scanner;
+
+import com.qa.Stages.StartStage;
+
+public class Runner {
+	static Scanner sc = new Scanner(System.in);
+//	static Scanner sc1 = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What is your name?");
+		String name = sc.nextLine();
+		System.out.println("Welcome to the swamp, " + name +
+					"!! You best be prepared!");
+		StartStage x = new StartStage();
+		String action = "";
+		x.Start(name, action);
+	}
+
+}
